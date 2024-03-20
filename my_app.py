9e913df627f9d95e76db92a1072b846ace1f5162
Alena25Bu <164194121+Alena25Bu@users.noreply.github.com>
2024-03-20 19:12:31 +0500

    Edit files

---
+++ my_app.py
@@ -0,0 +1,8 @@
+# напиши здесь код основного приложения и первого экрана
+from PyQt5.Qtcore import Qt
+from PyQt5.QtWidgets import(
+    QApplication, QWidget, 
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+    
